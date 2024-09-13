86ac651c14658dc698f5682789580e2a0bd9f4d3
Elijah <elijah.olson123@gmail.com>
2024-09-13 16:58:33 -0700

    punishment complete

--- Lab1/punishment.c
+++ Lab1/punishment.c
@@ -1,13 +1,39 @@
 #include <stdio.h>
 int main(){
     int numOfTimes;
+    int validInput;
+    int typoLine;
 
-    printf("How many times shall I write my punishment: ");
-    scanf("%d", &numOfTimes);
+    do{
+        printf("Enter the repetition count for the punishment phrase: ");
+        validInput = scanf("%d", &numOfTimes);
+        
+        while(getchar() != '\n');
+        if (validInput != 1 || numOfTimes <= 0) {
+            printf("You entered an invalid value for the repetition count! Please Re-");
+            validInput = 0;     
+        }
 
-    for(int i = 0; i < numOfTimes + 1; i++){
-        printf("Coding with C is awesome!\n") ;
+    } while (!validInput);
+
+    do{
+        printf("Enter the line where you want to insert the typo: ");
+        validInput = scanf("%d", &typoLine);
+
+        while(getchar() != '\n');
+        if (validInput != 1 || typoLine < 1 || typoLine > numOfTimes) {
+            printf("You entered an invalid value for the typo placement! Please Re-");
+            validInput = 0;     
+        }
+    } while (!validInput);
+
+    for(int i = 1; i <= numOfTimes; i++){
+        if(i == typoLine){
+            printf("Cading wiht is C avesone!\n");
+        } else{
+            printf("Coding with C is awesome!\n") ;
+        }
     }
-
+    
     return 0;
 }
