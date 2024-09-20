12f6b1b0cd623a0d9699d3a870234b53825211a5
Elijah <elijah.olson123@gmail.com>
2024-09-20 14:54:37 -0700

    final punishmnet

--- Lab1/punishment.c
+++ Lab1/punishment.c
@@ -1,31 +1,43 @@
 #include <stdio.h>
 int main(){
+
+    //to compile gcc -o punishment punishment.c
+    //to run ./punishment
+
     int numOfTimes;
     int validInput;
     int typoLine;
 
     do{
-        printf("Enter the repetition count for the punishment phrase: ");
+        if (validInput != 0){
+            printf("Enter the repetition count for the punishment phrase: ");
+        }
         validInput = scanf("%d", &numOfTimes);
         
         while(getchar() != '\n');
         if (validInput != 1 || numOfTimes <= 0) {
-            printf("You entered an invalid value for the repetition count! Please Re-");
+            printf("You entered an invalid value for the repetition count! Please re-enter: ");
             validInput = 0;     
         }
 
     } while (!validInput);
 
+    printf("\n");
+
     do{
-        printf("Enter the line where you want to insert the typo: ");
+        if (validInput != 0) {
+            printf("Enter the line where you want to insert the typo: ");
+        }
         validInput = scanf("%d", &typoLine);
 
         while(getchar() != '\n');
         if (validInput != 1 || typoLine < 1 || typoLine > numOfTimes) {
-            printf("You entered an invalid value for the typo placement! Please Re-");
+            printf("You entered an invalid value for the typo placement! Please re-enter: ");
             validInput = 0;     
-        }
+        } 
     } while (!validInput);
+
+    printf("\n");
 
     for(int i = 1; i <= numOfTimes; i++){
         if(i == typoLine){
